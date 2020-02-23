a4af3e7f6ae3ee2c5fb4b9f5b32c1204cf5173ea
Forrest Guice <forrestguice@users.noreply.github.com>
2020-02-23 11:37:14 -0700

    fab

--- app/src/main/java/com/forrestguice/suntimes/solunar/MainActivity.java
+++ app/src/main/java/com/forrestguice/suntimes/solunar/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.res.Resources;
 import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -41,6 +42,8 @@ public class MainActivity extends AppCompatActivity
     private RecyclerView cardView;
     private SolunarCardAdapter cardAdapter;
 
+    private FloatingActionButton fab;
+
     @Override
     protected void attachBaseContext(Context context)
     {
@@ -59,6 +62,9 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        fab = (FloatingActionButton)findViewById(R.id.fab);
+        fab.setOnClickListener(fabOnClickListener);
 
         cardView = (RecyclerView)findViewById(R.id.cardView);
         cardView.setHasFixedSize(true);
@@ -89,67 +95,9 @@ public class MainActivity extends AppCompatActivity
 
     protected void initData()
     {
-
-
         double latitude = Double.parseDouble(suntimesInfo.location[1]);
         double longitude = Double.parseDouble(suntimesInfo.location[2]);
         double altitude = Double.parseDouble(suntimesInfo.location[3]);
-
-        /**SolunarData data = new SolunarData(Calendar.getInstance().getTimeInMillis(), latitude, longitude, altitude, suntimesInfo.timezone);
-
-        SolunarCalculator calculator = new SolunarCalculator();
-        calculator.calculateData(getContentResolver(), data);
-
-        TextView text = (TextView)findViewById(R.id.text_debug);
-        text.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                AddonHelper.startSuntimesActivity(MainActivity.this);
-            }
-        });
-
-        if (data.isCalculated())
-        {
-            long sunrise = data.getDateMillis(SolunarData.KEY_SUNRISE);
-            long sunset = data.getDateMillis(SolunarData.KEY_SUNSET);
-            long moonrise = data.getDateMillis(SolunarData.KEY_MOONRISE);
-            long moonset = data.getDateMillis(SolunarData.KEY_MOONSET);
-
-            String debug = "date: " + SolunarCardHolder.formatDate(this, data.getDateMillis()) + "\n" +
-                    "timezone: " + data.getTimezone() + "\n" +
-                    "location: " + data.getLatitude() + ", " + data.getLongitude() + " [" + data.getAltitude() + "]\n\n" +
-                    "sunrise: " + SolunarCardHolder.formatTime(this, sunrise, suntimesInfo.timezone, false) + "\n" +
-                    "sunset: " + SolunarCardHolder.formatTime(this, sunset, suntimesInfo.timezone, false) + "\n\n" +
-                    "moonrise: " + SolunarCardHolder.formatTime(this, moonrise, suntimesInfo.timezone, false) + "\n" +
-                    "moonset: " + SolunarCardHolder.formatTime(this, moonset, suntimesInfo.timezone, false) + "\n" +
-                    "moonillum: " + data.getMoonIllumination() + "\n\n" +
-                    "rating: " + data.getDayRating();
-
-            debug += "\n\n" + "minor periods:\n";
-            SolunarPeriod[] minorPeriods = data.getMinorPeriods();
-            for (int i=0; i<minorPeriods.length; i++)
-            {
-                if (minorPeriods[i] != null) {
-                    debug += SolunarCardHolder.formatTime(this, minorPeriods[i].getStartMillis(), suntimesInfo.timezone, false) + " - " + SolunarCardHolder.formatTime(this, minorPeriods[i].getEndMillis(), suntimesInfo.timezone, false) + "\n";
-                }
-            }
-
-            debug += "\n\n" + "major periods:\n";
-            SolunarPeriod[] majorPeriods = data.getMajorPeriods();
-            for (int i=0; i<majorPeriods.length; i++)
-            {
-                if (majorPeriods[i] != null) {
-                    debug += SolunarCardHolder.formatTime(this, majorPeriods[i].getStartMillis(), suntimesInfo.timezone, false) + " - " + SolunarCardHolder.formatTime(this, majorPeriods[i].getEndMillis(), suntimesInfo.timezone, false) + "\n";
-                }
-            }
-
-            text.setText(debug);
-
-        } else {
-            text.setText("not calculated");
-        }*/
-
-
         cardAdapter = new SolunarCardAdapter(this, latitude, longitude, altitude, suntimesInfo.timezone);
         cardAdapter.setCardAdapterListener(cardListener);
 
@@ -163,6 +111,14 @@ public class MainActivity extends AppCompatActivity
         public void onDateClick(int i) {
             Snackbar.make(cardView, "date clicked " + i, Snackbar.LENGTH_LONG).setAction("TODO", null).show();
             // TODO
+        }
+    };
+
+    private View.OnClickListener fabOnClickListener = new View.OnClickListener()
+    {
+        @Override
+        public void onClick(View v) {
+            showToday();
         }
     };
 
