da30c4b941a09978c72d03353b5cbc782566e5c1
Forrest Guice <forrestguice@users.noreply.github.com>
2020-03-04 11:12:59 -0700

    copyright
    Copyright 2020 Forrest Guice
    SPDX-License-Identifier: GPL-3.0-or-later

--- app/src/main/java/com/forrestguice/suntimes/calculator/MoonPhaseDisplay.java
+++ app/src/main/java/com/forrestguice/suntimes/calculator/MoonPhaseDisplay.java
@@ -1,19 +1,20 @@
-/**
-    Copyright (C) 2018-2020 Forrest Guice
-    This file is part of SuntimesWidget.
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+    Copyright (C) 2020 Forrest Guice
+    This file is part of SolunarPeriods.
 
-    SuntimesWidget is free software: you can redistribute it and/or modify
+    SolunarPeriods is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    SuntimesWidget is distributed in the hope that it will be useful,
+    SolunarPeriods is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with SuntimesWidget.  If not, see <http://www.gnu.org/licenses/>.
+    along with SolunarPeriods.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 package com.forrestguice.suntimes.calculator;
