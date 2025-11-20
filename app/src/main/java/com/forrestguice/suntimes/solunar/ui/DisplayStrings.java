f1f6813d9240bbdffdc41d09e715b52d130540b8
Forrest Guice <forrestguice@users.noreply.github.com>
2025-11-19 21:48:01 -0700

    lint

--- app/src/main/java/com/forrestguice/suntimes/solunar/ui/DisplayStrings.java
+++ app/src/main/java/com/forrestguice/suntimes/solunar/ui/DisplayStrings.java
@@ -296,9 +296,12 @@ public class DisplayStrings
 
     public static Spanned fromHtml(String htmlString )
     {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
             return Html.fromHtml(htmlString, Html.FROM_HTML_MODE_LEGACY);
-        else return Html.fromHtml(htmlString);
+        } else {
+            //noinspection deprecation
+            return Html.fromHtml(htmlString);
+        }
     }
 
     /**
